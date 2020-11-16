c474000b8e852d8644a0a1ccb850c91a6120ff0d
LogLaDog <37880738+LogLaDog@users.noreply.github.com>
2020-11-15 17:03:33 -0700

    Fixed EmployeeHelpOnlyIssuesOneQuery

--- src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
+++ src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
@@ -4,32 +4,35 @@ import edu.montana.csci.csci440.model.Employee;
 import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
 public class EmployeeHelper {
     public static String makeEmployeeTree() {
         Employee employee = Employee.find(1); // root employee
         // and use this data structure to maintain reference information needed to build the tree structure
-        Map<Long, List<Employee>> employeeMap = new HashMap<>();
 
-        Jedis redis = new Jedis();
-        String str = redis.get("cs440-employee-tree-cache");
-        if (str == null){
-            str = "<ul>" + makeTree(employee, employeeMap) + "</ul>";
-            redis.set("cs440-employee-tree-cache", str);
+        Map<Long, List<Employee>> reportsMap = new HashMap<>();
+        for (Employee emp : Employee.all()) {
+            long reportsTo = emp.getReportsTo();
+            List<Employee> employeeReportsList = reportsMap.get(reportsMap);
+            if (employeeReportsList == null) {
+                employeeReportsList = new LinkedList<>();
+                reportsMap.put(reportsTo, employeeReportsList);
+            }
+            employeeReportsList.add(emp);
         }
-        return str;
+        return "<ul>" + makeTree(employee, reportsMap)+ "<ul>";
     }
 
-    // TODO - currently this method just usese the employee.getReports() function, which
-    //  issues a query.  Change that to use the employeeMap variable instead
     public static String makeTree(Employee employee, Map<Long, List<Employee>> employeeMap) {
         String list = "<li><a href='/employees" + employee.getEmployeeId() + "'>"
                 + employee.getEmail() + "</a><ul>";
-        List<Employee> reports = employee.getReports();
-        for (Employee report : reports) {
-            list += makeTree(report, employeeMap);
+        List<Employee> reports = employeeMap.get(employee);
+        if(reports != null) {
+            for (Employee report : reports) {
+                list += makeTree(report, employeeMap);
+            }
         }
         return list + "</ul></li>";
     }
